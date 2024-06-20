a1c05a90e0cd066e7d7227342d8894da56a07703
Bhavesh Jain <bhaveshkocheta50@gmail.com>
2024-06-20 18:04:28 +0530

    Update tailwind.config.json

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#383838'
+      },
+      margin: {
+        'mbe': '16px'
+      }
+    },
   },
   plugins: [],
 }
